3c867d9f1eaf3b4c4b316dcf30f05c09fe685475
benjamin <benjamin.huete.n@gmail.com>
2023-02-08 14:03:04 -0300

    correccion en api

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"production"',
   address: {
     base: '"https://testapi.acamy.cl/"',
-    api: '"https://api.corretaje.site"',
+    api: '"https://api.corretaje.site/"',
     spa: '"http://localhost:8080"',
     socket: '"http://localhost:6001"',
     apiBot: '"https://api.dialogflow.com/v1/"',
